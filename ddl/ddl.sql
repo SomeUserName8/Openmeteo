0ce3d495ebfa19fb222a5f7caa579a13dea1b4f0
Nikita Popov <nikitos-popov@yandex.ru>
2025-02-27 23:56:04 +0300

    feat: minor fix

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,12 +1,12 @@
 create schema stg;
 
-drop table stg.historical_data;
-create table stg.historical_data(
+drop table if exists stg.historical_data;
+create table IF NOT EXISTS stg.historical_data(
 upload_date timestamp not null,
 data JSON not null);
 
-drop table stg.forecast_data;
-create table stg.forecast_data(
+drop table if exists stg.forecast_data;
+create table if IF NOT EXISTS stg.forecast_data(
 upload_date timestamp not null,
 data JSON not null);
 
